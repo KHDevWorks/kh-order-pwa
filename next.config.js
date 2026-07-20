0faea1d14187db742a888453bcdc2c78dd4896f1
Kouno Hiroaki <kouno1219hiroaki0407@gmail.com>
2026-07-20 20:46:18 +0900

    修正

--- next.config.js
+++ next.config.js
@@ -20,5 +20,13 @@ module.exports = withPWA({
 >>>>>>> eaed134 (2026.4.13変更)
 =======
   output: 'standalone',
+<<<<<<< HEAD
 >>>>>>> f3979d6 (docker環境)
 });
+=======
+});
+
+module.exports = {
+  output: 'export',
+};
+>>>>>>> 4284093 (修正)
